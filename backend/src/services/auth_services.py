ad0a6ed55f40ddd8cf239a43c7435e570a53cc18
Tapoban Ray <raytapoban@gmail.com>
2026-02-16 20:49:05 +0530

    Added logs in auth services to figure out auth issue in release build.

--- backend/src/services/auth_services.py
+++ backend/src/services/auth_services.py
@@ -22,7 +22,6 @@ from src.exceptions import (
 from src.models.user_model import UserProfileDataModel
 from src.repositories.user_auth_repository import UserAuthRepository
 from src.schemas.auth_schemas import UserProfileDetailsSchema
-import logging
 
 from src.services.user_credits_and_quotas_services import create_new_credits_profile
 
@@ -32,8 +31,6 @@ firebase_admin.initialize_app(cred)
 SECRET_KEY = JWT_SECRET_KEY
 ALGORITHM = JWT_HASH_ALGORITHM
 ACCESS_TOKEN_EXPIRE_MINUTES = int(EXPIRE_MINUTES_ACCESS_TOKEN)
-auth_logger = logging.getLogger()
-auth_logger.setLevel(logging.DEBUG)
 
 auth_repo = UserAuthRepository()
 
@@ -45,6 +42,9 @@ def sign_in_user_service(token: str):
      - Generates and returns access key.
      Else, raise 401 error"""
     try:
+        print("RECEIVED GOOGLE ID TOKEN. FETCHING CLAIMS.")
+        app = firebase_admin.get_app()
+        print(app.credential)
         claims = auth.verify_id_token(token, clock_skew_seconds=5)
         user_details: UserProfileDetailsSchema = (
             UserProfileDetailsSchema.model_validate(claims)
@@ -52,16 +52,19 @@ def sign_in_user_service(token: str):
 
         user = auth_repo.get_user(user_id=user_details.user_id)
 
-        auth_logger.info(user)
+        print(user)
         if not user:
+            print("CREATING NEW USER.")
             new_user = UserProfileDataModel.model_validate(user_details.model_dump())
             auth_repo.create_user(new_user)
             create_new_credits_profile(new_user.user_id)
+            print(new_user)
 
         access_token = generate_jwt_token(
             user_details.user_id,
             expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
         )
+        print(access_token)
         return {"access_token": access_token}
 
     except auth.InvalidIdTokenError as e:
